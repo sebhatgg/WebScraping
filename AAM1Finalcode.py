d669e358e243c5550683e30a6007da59bbd9ce85
sebhatgg <sebhatgg@gmail.com>
2020-10-24 10:25:22 -0400

    Absolute links

--- AAM1Finalcode.py
+++ AAM1Finalcode.py
@@ -6,27 +6,19 @@ import urllib
 import csv
 
 r = requests.get('https://www.census.gov/programs-surveys/popest.html')
-
-
-# In[15]:
-
-
 soup = BeautifulSoup(r.text, 'html.parser')
 #for link in soup.find_all('a'):
    # print (link.get('href'))
 links = soup.find_all('a')
 print(len(links))
-
-
-# In[24]:
-
+# Only the ablsolute links
 
 sg = soup.findAll('a', attrs={'href': re.compile("^http")})
 print(sg)
 
-
-# In[25]:
-
-
 print(len(sg))
 
+with open("aam1taskRev.csv",'w') as csvfile:
+    write = csv.writer(csvfile, delimiter = ' ')
+    write.writerows(sg)
+    csvfile.close()
